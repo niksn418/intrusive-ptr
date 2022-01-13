10ab56cb63c8269eb846bb2691df1cbdb3debad6
Zakhar Koval <ellesterate@gmail.com>
2022-01-13 22:12:53 +0300

    Updated task readme

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# Template for tasks of C++ course
+# intrusive-ptr
 
-> NOTE: Change project name in CMakeLists.txt
+Вам нужно реализовать 
+[intrusive_ptr](https://www.boost.org/doc/libs/1_71_0/libs/smart_ptr/doc/html/smart_ptr.html#intrusive_ptr) 
+и [intrusive_ref_counter](https://www.boost.org/doc/libs/1_71_0/libs/smart_ptr/doc/html/smart_ptr.html#intrusive_ref_counter), 
+а также некоторые вспомогательные утилиты (операторы, API счётчика).
+
+Обратите внимание, что `intrusive_ref_counter` должен иметь единственную политику потокобезопасности, аналогичную `boost::thread_safe_counter`.
--- solution.h
+++ solution.h
@@ -1,8 +1,90 @@
 #include <boost/smart_ptr/intrusive_ptr.hpp>
 #include <boost/smart_ptr/intrusive_ref_counter.hpp>
 
-template<typename T>
-using intrusive_ptr = boost::intrusive_ptr<T>;
+// TODO: remove after finishing tests
+//template<typename T>
+//using intrusive_ptr = boost::intrusive_ptr<T>;
+//
+//template<typename T>
+//using intrusive_ref_counter = boost::intrusive_ref_counter<T>;
 
 template<typename T>
-using intrusive_ref_counter = boost::intrusive_ref_counter<T>;
+struct intrusive_ptr {
+  using element_type = T;
+
+  intrusive_ptr() noexcept;
+  intrusive_ptr(T * p, bool add_ref = true);
+
+  intrusive_ptr(intrusive_ptr const & r);
+  template<class Y> intrusive_ptr(intrusive_ptr<Y> const & r);
+
+  intrusive_ptr(intrusive_ptr && r);
+  template<class Y> intrusive_ptr(intrusive_ptr<Y> && r);
+
+  ~intrusive_ptr();
+
+  intrusive_ptr & operator=(intrusive_ptr const & r);
+  template<class Y> intrusive_ptr & operator=(intrusive_ptr<Y> const & r);
+  intrusive_ptr & operator=(T * r);
+
+  intrusive_ptr & operator=(intrusive_ptr && r);
+  template<class Y> intrusive_ptr & operator=(intrusive_ptr<Y> && r);
+
+  void reset();
+  void reset(T * r);
+  void reset(T * r, bool add_ref);
+
+  T & operator*() const noexcept;
+  T * operator->() const noexcept;
+  T * get() const noexcept;
+  T * detach() noexcept;
+
+  explicit operator bool () const noexcept;
+
+  void swap(intrusive_ptr & b) noexcept;
+};
+
+template<class T, class U>
+bool operator==(intrusive_ptr<T> const & a, intrusive_ptr<U> const & b) noexcept;
+
+template<class T, class U>
+bool operator!=(intrusive_ptr<T> const & a, intrusive_ptr<U> const & b) noexcept;
+
+template<class T, class U>
+bool operator==(intrusive_ptr<T> const & a, U * b) noexcept;
+
+template<class T, class U>
+bool operator!=(intrusive_ptr<T> const & a, U * b) noexcept;
+
+template<class T, class U>
+bool operator==(T * a, intrusive_ptr<U> const & b) noexcept;
+
+template<class T, class U>
+bool operator!=(T * a, intrusive_ptr<U> const & b) noexcept;
+
+template<class T>
+bool operator<(intrusive_ptr<T> const & a, intrusive_ptr<T> const & b) noexcept;
+
+template<class T> void swap(intrusive_ptr<T> & a, intrusive_ptr<T> & b) noexcept;
+
+template<typename T>
+struct intrusive_ref_counter {
+  intrusive_ref_counter() noexcept;
+  intrusive_ref_counter(const intrusive_ref_counter& v) noexcept;
+
+  intrusive_ref_counter& operator=(const intrusive_ref_counter& v) noexcept;
+
+  unsigned int use_count() const noexcept;
+
+protected:
+  ~intrusive_ref_counter() = default;
+};
+
+template<class Derived>
+void intrusive_ptr_add_ref(
+    const intrusive_ref_counter<Derived>* p) noexcept;
+
+template<class Derived>
+void intrusive_ptr_release(
+    const intrusive_ref_counter<Derived>* p) noexcept;
+
